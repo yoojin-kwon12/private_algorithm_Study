6a27ee60573230e996d247c86f98937e96c087f0
kwon yoojin <kyj2933827@naver.com>
2022-01-04 12:14:02 +0900

    20210104

---
+++ Queue/10845.py
@@ -0,0 +1,72 @@
+# 10845번
+
+"""
+# 시작 체크 리스트
+1. 1시간이 지났으나 발상 불가 또는 아예 다른 길
+2. 코드 50% 정도 완성
+3. 1시간 보다 더 걸려서 코드 완성.
+4. 코드는 다 돌아가는데 효율성에서 걸림.
+5. 코드완성
+
+# 완료 후 체크 리스트
+1. 아예 모르겠음
+2. 중간 정도 이해함.
+3. 완벽히 이해함.
+"""
+import sys
+
+n = int(input())
+inp = []
+result = []
+
+for i in range(n):
+    inp = sys.stdin.readline().split()
+    if inp[0] == 'push':
+        result.append(int(inp[1]))
+    elif inp[0] == 'pop':
+        if len(result) == 0:
+            print(-1)
+        else:
+            print(result.pop(0))
+    elif inp[0] == 'size':
+        print(len(result))
+    elif inp[0] == 'empty':
+        if len(result) == 0:
+            print(1)
+        else:
+            print(0)
+    elif inp[0] == 'front':
+        if len(result) == 0:
+            print(-1)
+        else:
+            print(result[0])
+    elif inp[0] == 'back':
+        if len(result) == 0:
+            print(-1)
+        else:
+            print(result[len(result)-1])
+    else:
+        print('명령어를 잘못입력하였습니다.')
+
+
+# print(inp)
+
+
+
+"""
+push 1
+push 2
+1 -> front
+2 -> back
+2 -> size
+0 -> empty
+1 -> pop
+2 -> pop
+-1 -> pop
+0 -> size
+1 -> empty
+-1 -> pop
+push 3
+0 -> empty
+3 -> front
+"""
